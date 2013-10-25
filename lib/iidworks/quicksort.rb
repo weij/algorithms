b98478c33a0f1008a1097756ef370d095f0e080c
Weijun Yin <gw9812@gmail.com>
2013-10-25 10:15:33 +0800

    removed redundent test

--- lib/iidworks/quicksort.rb
+++ lib/iidworks/quicksort.rb
@@ -1,13 +1,13 @@
 module IIDWorks
   module QuickSort
-	def self.sort_internal(collection, lo, hi, cutoff: 5)
-	  return insertion_sort(collection, lo, hi) if hi <= lo + cutoff
-	  j = partition(collection, lo, hi)
-      sort_internal(collection, lo, j-1)
-      sort_internal(collection, j+1, hi)   
-	end	
+	  def self.sort_internal(collection, lo, hi, cutoff: 5)
+	    return insertion_sort(collection, lo, hi) if hi <= lo + cutoff
+	    j = partition(collection, lo, hi)
+        sort_internal(collection, lo, j-1)
+        sort_internal(collection, j+1, hi)   
+	  end	
 
-	def self.partition(collection, lo, hi)
+	  def self.partition(collection, lo, hi)
       i, j = lo, hi+1
       v = collection[lo]
       loop do 
@@ -16,25 +16,22 @@ module IIDWorks
           break if i == hi
           i += 1
       	end
-
-      	j -= 1
+     	j -= 1
       	while v < collection[j]
           break if j == lo
           j -= 1
       	end
-
-      	# puts "#{i} >= #{j}" 
+     	# puts "#{i} >= #{j}" 
         break if i >= j
         # puts "From left (i): #{collection[i]} at #{i}; from right (j): #{collection[j]} at #{j}"
       	collection[i], collection[j] = collection[j], collection[i]
         # puts collection.to_s
       end
-
-      # puts "#{collection[j]} at #{j} exchange with #{collection[lo]} at #{lo}"
+     # puts "#{collection[j]} at #{j} exchange with #{collection[lo]} at #{lo}"
       collection[lo], collection[j] = collection[j], collection[lo]
       # puts collection.to_s
       return j
-	end
+	  end
 
     def self.insertion_sort(collection, lo, hi)
       j = lo + 1
